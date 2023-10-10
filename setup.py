74276af91e85937e9b734d013424d7cada03f92a
NukP <nukorn.p27@gmail.com>
2023-10-10 14:16:49 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sxrd",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here, e.g.
+        # 'numpy>=1.20.0'
+    ],
+)
