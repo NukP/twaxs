f4d964bece9abfc866f31bab56c8380e981f4e4a
NukP <nukorn.p27@gmail.com>
2023-10-11 14:16:30 +0200

    Add group_height to the Dataset class

--- sxrd/auxiliary.py
+++ sxrd/auxiliary.py
@@ -1,13 +1,20 @@
 import h5py
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-from ipywidgets import interactive, IntSlider, SelectionSlider
-from IPython.display import clear_output
-import plotly.graph_objects as go
-import numpy as np
-import pandas as pd
-import os
-from scipy.signal import savgol_filter
-from dateutil.parser import parse
-import math
-import typing
+from typing import Any
+''''    
+This module contains functions that helps analyze the raw data which can be used by the plotting functions in the plot module. 
+'''
+
+def get_data(fln: str, dataset_path: str) -> Any:
+    '''
+    Extract data from a specified hdf5 file and dataset path.
+
+    Parameters:
+    fln (str): The file path for the hdf5 file.
+    dataset_path (str): The specific dataset path within the hdf5 file.
+
+    Returns:
+    Any: The data found at the specified dataset path within the file.
+    '''
+    with h5py.File(fln, 'r') as f:
+        data = f[dataset_path][()]
+    return data
--- sxrd/dataset.py
+++ sxrd/dataset.py
@@ -1,9 +1,12 @@
 import os
-from typing import Dict, Tuple
+import h5py
+from typing import Dict, Tuple, Any
+from . import auxiliary as aux
 """   
 This module contains functions that load and handles parsing of the raw data
 """  
 class Dataset:
+   
     dict_fln: Dict[str, str] = {
         '6': '06_s01',
         '11': '11_s01',
@@ -30,11 +33,13 @@ class Dataset:
         '19': 6
     }
 
-    def __init__(self, fln: int):
+    def __init__(self, fln: int, height_group: int):
         self.fln: str = str(fln)
+        self.height_group: int = height_group
         self._fln_raw: str
         self._fln_integrated: str
         self._fln_start_macro: int
+        self._height_group_frame: Dict[int, Any] = {}  # Initialize the variable, will be populated later
         self._fln_raw, self._fln_integrated, self._fln_start_macro = self.get_fln_detail()
 
     def get_fln_detail(self) -> Tuple[str, str, int]:
@@ -45,7 +50,7 @@ class Dataset:
         return fln_raw, fln_integrated, fln_start_macro
 
     @property
-    def file_name(self) -> str:
+    def fln_raw(self) -> str:
         return self._fln_raw
 
     @property
@@ -53,5 +58,46 @@ class Dataset:
         return self._fln_integrated
 
     @property
-    def start_macro(self) -> int:
+    def fln_start_macro(self) -> int:
         return self._fln_start_macro
+
+    def get_max_frame_index(self, filename: str) -> int:
+        with h5py.File(filename, 'r') as f:
+            keys = list(f.keys())
+            frame_indices = [int(key.split('.')[0]) for key in keys if '.' in key]
+            return max(frame_indices)
+
+    def get_height_array(self, start: int, num_end: int) -> Dict[int, Any]:
+        end = self.get_max_frame_index(self._fln_raw) if num_end == 0 else num_end
+        dict_height = {}
+        for scan_num in range(start, end+1):
+            height = aux.get_data(fln=self._fln_raw, dataset_path=f'{scan_num}.1/instrument/positioners/h1tz')
+            dict_height[scan_num] = height
+        return dict_height
+
+    @property
+    def height_group_frame(self) -> Dict[int, Any]:
+        if not self._height_group_frame:
+            end_num = 148 if self.fln == '12' else 0
+            height_array = self.get_height_array(self._fln_start_macro, end_num)
+            grouped_height_array = group_heights(height_array)
+            self._height_group_frame = grouped_height_array[self.height_group]
+        return self._height_group_frame
+    
+def group_heights(h_array: Dict[int,int]) -> Dict[int,int]: 
+    '''
+    This function sort the peak the number of peak position at to 4 groups (3 experimental height and 1 controlled height)
+    '''
+    rounded_h_array = {k: round(v, 3) for k, v in h_array.items()}
+    groups = {}
+    for scan_num, height in rounded_h_array.items():
+        if height not in groups:
+            groups[height] = []
+        groups[height].append(scan_num)
+    unique_heights = sorted(groups.keys())
+    h_list = [groups[height] for height in unique_heights]
+    control_height = min(unique_heights, key=lambda x: len(groups[x]))
+    for i, height in enumerate(unique_heights):
+        height_type = "control height" if height == control_height else f"height {i}"
+        #print(f"list position {i}; {height_type} at {height} mm")
+    return h_list
