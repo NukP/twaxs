6b3387117f13c19d74fb2883b4e944e8e059e8b6
NukP <nukorn.p27@gmail.com>
2023-10-25 10:44:32 +0200

    Fix this setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sxrd",
-    version="1.0.0",
+    version="1.1.2",
     packages=find_packages(),
     install_requires=[
     'h5py',
