1b2e26459a7f6a182e51d04804d5901f5d0b1009
NukP <nukorn.p27@gmail.com>
2023-11-09 22:55:46 +0100

    commit to prepare to add to version 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sxrd",
-    version="1.1.2",
+    version="1.2.0",
     packages=find_packages(),
     install_requires=[
     'h5py',
