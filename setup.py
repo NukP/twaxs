45b4ab2e47bc4014ce2d6e2c82786bbb5edcbc71
NukP <127328032+NukP@users.noreply.github.com>
2024-03-21 10:29:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sxrd",
-    version="1.2.0",
+    name="twaxs",
+    version="2.0.0",
     packages=find_packages(),
     install_requires=[
     'h5py',
