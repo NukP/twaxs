da5883b2ec3187d0d96b077e1306339b922a4c95
NukP <127328032+NukP@users.noreply.github.com>
2024-03-21 16:52:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="twaxs",
     version="2.0.0",
-    packages=find_packages(),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     install_requires=[
     'h5py',
     'matplotlib',
