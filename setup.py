eebc51b78a3a55801ae7dd1b0699024a420c46d9
NukP <127328032+NukP@users.noreply.github.com>
2024-03-22 11:05:03 +0100

    Add author name and author email in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="twaxs",
     version="2.0.0",
     package_dir={'': 'src'},
+    author="Nukorn Plainpan",
+    author_email="nukorn.p27@gmail.com",
     packages=find_packages(where='src'),
     install_requires=[
     'h5py',
